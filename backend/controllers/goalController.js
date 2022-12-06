43925a9bd5fddb7b23b514c7758c86af8c71cdc1
SwapnilRamgire <swapnilramgire007@gmail.com>
2022-12-06 12:12:15 +0530

    added verification for object ids

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,4 +1,6 @@
 import { Goal } from "../models/goalModel.js";
+import mongoose from "mongoose";
+const ObjectId = mongoose.Types.ObjectId;
 
 const getGoal = async (req, res) => {
   const goals = await Goal.find();
@@ -16,19 +18,23 @@ const postGoal = async (req, res) => {
 };
 
 const putGoal = async (req, res) => {
-  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-  if (!updatedGoal)
-    return res.status(404).send(`Goal with id ${req.params.id} not found`);
-  res.send(updatedGoal);
+  if (ObjectId.isValid(req.params.id)) {
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!updatedGoal)
+      return res.status(404).send(`Goal with id ${req.params.id} not found`);
+    res.send(updatedGoal);
+  } else return res.status(400).send(`Provide proper id`);
 };
 
 const deleteGoals = async (req, res) => {
-  const deletedGoal = await Goal.findOneAndDelete(req.params.id);
-  if (!deletedGoal)
-    return res.status(404).send(`Goal with id ${req.params.id} not found`);
-  res.send(deletedGoal);
+  if (ObjectId.isValid(req.params.id)) {
+    const deletedGoal = await Goal.findByIdAndDelete(req.params.id);
+    if (!deletedGoal)
+      return res.status(404).send(`Goal with id ${req.params.id} not found`);
+    res.send(deletedGoal);
+  } else return res.status(400).send(`Provide proper id`);
 };
 
 export { getGoal, postGoal, putGoal, deleteGoals };
