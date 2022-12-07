4637b92236d294c415743b721722e7dd8534f255
SwapnilRamgire <swapnilramgire007@gmail.com>
2022-12-07 19:42:11 +0530

    added user register functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,37 @@
-const registereUser = (req, res) => {
-  res.send("User registered");
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import { User } from "../models/userModel.js";
+
+const registereUser = async (req, res) => {
+  const { userName, password } = req.body;
+  if (!userName || !password) {
+    return res.status(400).send("Please provide all the details");
+  }
+  const findUser = await User.findOne({ userName });
+  if (findUser) return res.status(400).send("User already exists");
+
+  const saltShit = await bcrypt.genSalt(10);
+  const hashedPasswaord = await bcrypt.hash(password, saltShit);
+
+  const user = await User.create({
+    userName: userName,
+    password: hashedPasswaord,
+  });
+  if (user) {
+    return res.status(201).json({
+      _id: user.id,
+      userName: user.userName,
+    });
+  } else {
+    return res.status(400).send("Invalid user data and shit");
+  }
 };
 
-const loginUser = (req, res) => {
+const loginUser = async (req, res) => {
   res.send("User login");
 };
 
-const getMe = (req, res) => {
+const getMe = async (req, res) => {
   res.send("User data");
 };
 
